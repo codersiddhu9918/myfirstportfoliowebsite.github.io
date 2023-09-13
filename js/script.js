028130a4d3813f3a360d853f057999d614f15d43
codersiddhu <dwivedisiddharth1999@gmail.com>
2023-09-14 02:21:52 +0530

    updated
    updated about and added read more

--- js/script.js
+++ js/script.js
@@ -62,3 +62,22 @@ ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact for
         loop:true
 
     });
+<<<<<<< Updated upstream
+=======
+
+    function myFunction() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more");
+        var btnText = document.getElementById("myBtn");
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "Read more";
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = "Read less";
+          moreText.style.display = "inline";
+        }
+      }
+>>>>>>> Stashed changes
